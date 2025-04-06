c4018c67c9f59c3a02271f61388d830b810b808f
Victor Bala <savicsly@gmail.com>
2025-04-06 23:43:51 +0100

    feat: add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('FX Trading App')
+    .setDescription('The FX Trading App API description')
+    .setVersion('1.0')
+    .addTag('fx')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
